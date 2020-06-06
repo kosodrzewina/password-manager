83e2514cbe168182f0edbdf2b7e7ab795e29ca0b
Kosodrzewinatru <sienkiew@live.com>
2020-06-06 22:45:54 +0200

    added hash namespace

--- password-manager/password_manager.hpp
+++ password-manager/password_manager.hpp
@@ -3,4 +3,12 @@
 namespace serde
 {
 	std::string serialize();
+	void deserialize(std::string);
+}
+
+namespace hash
+{
+	std::string encrypt(std::string, std::string);
+	std::string decrypt(std::string, std::string);
+	bool grant_access(std::string, std::string);
 }
