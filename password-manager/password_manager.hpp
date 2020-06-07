a2b949f0bd8cfca58d27124b68fae7b7c091b399
Kosodrzewinatru <sienkiew@live.com>
2020-06-07 23:24:59 +0200

    added Data structure in the header file

--- password-manager/password_manager.hpp
+++ password-manager/password_manager.hpp
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <fstream>
+#include <filesystem>
+#include <vector>
+
 namespace serde
 {
 	std::string serialize();
@@ -12,3 +16,17 @@ namespace hash
 	std::string decrypt(std::string, std::string);
 	bool grant_access(std::string, std::string);
 }
+
+struct Data
+{
+	std::fstream file_output;
+	std::ofstream file_input;
+	std::vector<std::vector<std::string>> credentials_list;
+
+public:
+	Data(std::fstream);
+	
+	void create_file(std::ofstream);
+	void save_credentials(std::string, std::string);
+	std::vector<std::vector<std::string>> get_list();
+};
